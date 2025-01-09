e572412f746cfd480cc55f18e27884c1ede4a8c2
David M <manca@spravazeleznic.cz>
2025-01-09 13:51:02 +0100

    clippy

--- crate/examples/rotate.rs
+++ crate/examples/rotate.rs
@@ -13,12 +13,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Seam Carver
     let (w, h) = (img.get_width(), img.get_height());
     println!("original = w: {}, h: {}", w, h);
-    let angles = vec![
-        60.0,  //   60.0 = q1:60.0
+    let angles = [60.0,  //   60.0 = q1:60.0
         135.0, //  135.0 = q2:45.0
         562.5, //  517.5 = q3:22.5
-        -30.0, //  -30.0 = q4:60.0
-    ];
+        -30.0];
     let operations = angles.len();
     let mut results = Vec::new();
     for i in 0..operations {
@@ -37,7 +35,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in (0..operations).rev() {
         photon_rs::native::save_image(
             results.remove(i),
-            &format!("output_rotate_{}.jpg", i + 1),
+            format!("output_rotate_{}.jpg", i + 1),
         )?;
     }
     let end = Instant::now();
